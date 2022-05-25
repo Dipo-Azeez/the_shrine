b6dca9619d590d2216411593ea1a880b6ec56b14
Azeez Dipo <oladipupoazeez0@gmail.com>
2022-05-25 09:02:10 -0700

    April

---
+++ general_js.js
@@ -0,0 +1,185 @@
+function myFunction(){
+    document.body.classList.toggle("mobile-nav-active")
+}
+
+function toggleText(){
+    var x = document.getElementById("demo");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText1(){
+    var x = document.getElementById("demo1");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+
+    }
+    
+function toggleText2(){
+    var x = document.getElementById("demo2");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText3(){
+    var x = document.getElementById("demo3");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText4(){
+    var x = document.getElementById("demo4");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText5(){
+    var x = document.getElementById("demo5");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText6(){
+    var x = document.getElementById("demo6");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText7(){
+    var x = document.getElementById("demo7");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText8(){
+    var x = document.getElementById("demo8");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText9(){
+    var x = document.getElementById("demo9");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText10(){
+    var x = document.getElementById("demo10");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText11(){
+    var x = document.getElementById("demo11");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText12(){
+    var x = document.getElementById("demo12");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText13(){
+    var x = document.getElementById("demo13");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText14(){
+    var x = document.getElementById("demo14");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText15(){
+    var x = document.getElementById("demo15");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText16(){
+    var x = document.getElementById("demo16");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText17(){
+    var x = document.getElementById("demo17");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText18(){
+    var x = document.getElementById("demo18");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
+function toggleText19(){
+    var x = document.getElementById("demo19");
+    if (x.style.display === "none" ) {
+        x.style.display = "block";
+    } else{
+        x.style.display = "none";
+    }
+    }
+
