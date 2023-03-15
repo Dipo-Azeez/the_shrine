83904a470aeda2c3a724ad01dd2a1b78dbb8b030
Azeez Dipo <oladipupoazeez0@gmail.com>
2023-03-14 17:00:10 -0700

    New changes and upgrades
    Modifications added

--- general_js.js
+++ general_js.js
@@ -183,3 +183,4 @@ function toggleText19(){
     }
     }
 
+    
---
+++ shrine_landing_page.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+    document.body.classList.toggle("mobile-nav-active")
+}
+
+
+function myFunctions() {
+    var x = document.getElementById();
+    x.style.display = "block";
+}
+
+function myFunctions() {
+    var y = document.getElementById();
+    y.style.display = "none";
+    var message = document.getElementById().value;
+    alert("Hey " + message + ", Thank you for subscribing")
+}
